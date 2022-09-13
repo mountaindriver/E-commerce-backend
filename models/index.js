f5604469ebe23ce349730d268475accb791bb811
Mountian Driver <lsfreigenberg@outlook.com>
2022-09-12 18:21:45 -0600

    modele associations

--- models/index.js
+++ models/index.js
@@ -4,29 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// FIX ALL OF THIS 13/24;
-
 // Products belongsTo Category
-Product.belongsToMany(Product, {
+Product.belongsTo(Category, {
   foreignKey: `category_id`,
 })
 
 // Categories have many Products
-Category.belongsToMany(Product, {
-  through: ProductTag,
+Category.hasMany(Product, {
   foreignKey: `category_id`,
 })
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Product, {
+Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: `tag_id`,
+  foreignKey: `product_id`,
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: `tag_id`,
 })
+
+
 
 module.exports = {
   Product,
